a6575c288c0522f3d4d0533ed917aff35df24dfc
enriquevergara1996 <envemo1996@gmail.com>
2025-05-08 14:34:25 +0200

    .

--- 42cursus-get_next_line/get_next_line.c
+++ 42cursus-get_next_line/get_next_line.c
@@ -12,7 +12,45 @@
 
 #include "get_next_line.h"
 
-char	*read_and_store(int fd, char *storage)
+static char	*ft_extract_line(char *storage)
+{
+	char	*line;
+	char	*newline;
+	size_t	len;
+
+	newline = ft_strchr(storage, '\n');
+	if (newline)
+	{
+		len = newline - storage + 1;
+		line = malloc(len + 1);
+		if (!line)
+			return (NULL);
+		ft_memcpy(line, storage, len);
+		line[len] = '\0';
+		return (line);
+	}
+	return (NULL);
+}
+
+static char	*ft_update_storage(char *storage)
+{
+	char	*newline;
+	char	*new_storage;
+	size_t	len;
+
+	newline = ft_strchr(storage, '\n');
+	if (newline)
+	{
+		len = newline - storage + 1;
+		new_storage = ft_strdup(storage + len);
+		free(storage);
+		return (new_storage);
+	}
+	free(storage);
+	return (NULL);
+}
+
+static char	*ft_read_and_store(int fd, char *storage)
 {
 	char	*buffer;
 	ssize_t	bytes_read;
@@ -27,51 +65,15 @@ char	*read_and_store(int fd, char *storage)
 		if (bytes_read < 0)
 		{
 			free(buffer);
+			free(storage);
 			return (NULL);
 		}
 		buffer[bytes_read] = '\0';
 		storage = ft_strjoin(storage, buffer);
-		if (ft_strchr(storage, '\n'))
+		if (!storage || ft_strchr(storage, '\n'))
 			break ;
 	}
 	free(buffer);
-	return (storage);
-}
-
-char	*extract_line(char *storage)
-{
-	char	*line;
-	char	*newline;
-	size_t	line_len;
-
-	newline = ft_strchr(storage, '\n');
-	if (newline)
-	{
-		line_len = newline - storage + 1;
-		line = malloc(line_len + 1);
-		if (!line)
-			return (NULL);
-		ft_memcpy(line, storage, line_len);
-		line[line_len] = '\0';
-		return (line);
-	}
-	return (NULL);
-}
-
-char	*update_storage(char *storage)
-{
-	char	*newline;
-	char	*new_storage;
-	size_t	line_len;
-
-	newline = ft_strchr(storage, '\n');
-	if (newline)
-	{
-		line_len = newline - storage + 1;
-		new_storage = ft_strdup(storage + line_len);
-		free(storage);
-		return (new_storage);
-	}
 	return (storage);
 }
 
@@ -82,10 +84,10 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	storage = read_and_store(fd, storage);
+	storage = ft_read_and_store(fd, storage);
 	if (!storage)
 		return (NULL);
-	line = extract_line(storage);
+	line = ft_extract_line(storage);
 	if (!line)
 	{
 		if (!storage || *storage == '\0')
@@ -97,8 +99,8 @@ char	*get_next_line(int fd)
 		line = ft_strdup(storage);
 		free(storage);
 		storage = NULL;
+		return (line);
 	}
-	else
-		storage = update_storage(storage);
+	storage = ft_update_storage(storage);
 	return (line);
 }
